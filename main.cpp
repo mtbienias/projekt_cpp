f245e0684f8152479bd784a87c8129aa25e6df0d
Mateusz Bienias <bieniasmt@gmail.com>
2022-11-03 15:19:09 +0100

    Pierwszy punkt projektu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+	double a,b;
+	float m0;
 	
-	cout << "hello world";
+	cout << "Program oblicza funkcje liniowa w postaci: ax + b gdzie a =/= 0" << endl;
+	cout << "Podaj a: ";  cin >> a;
+	cout << "Podaj b: ";  cin >> b;
+	
+	cout << "f(x) = " << a << "x + " << b << endl;
+	m0 = b/a;
+	cout << "Miejsce zerowe: " << m0 << endl;
+	
 	return 0;
 }
