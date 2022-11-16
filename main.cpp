e7024272fb0e172e1121fd48673d3d87e7628509
mtbienias <bieniasdevs@gmail.com>
2022-11-16 17:22:34 +0100

    v1.4 postac ogolna funkcji liniowej

--- main.cpp
+++ main.cpp
@@ -3,17 +3,36 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    float a, b, x;
+    float a, b, c, x;
+    int ktora_funkcja;
 
+    cout << "Wybierz postac funkcji liniowej (1/2): "<<endl;
+    cout << "1) y=ax+b"<<endl;
+    cout << "2) Ax + By + C = 0"<<endl;
+    cout << "Wybrana funkcja: "; cin >> ktora_funkcja;
 
-    cout << "Program do obliczania miejsc zerowych funkcji: y=ax+b" <<endl;
-    cout << "Podaj A: "; cin >> a;
-    cout << "Podaj B: "; cin >> b;
-    x=(-b/a);
-    if (a==0) {
-        cout << "Funkcja przyjmuje postac: y="<<b<<" wiec jest stala" <<endl;
-    }  else {
-        cout << "Miejsce zerowe funkcji y="<<a<<"x"<<"+b to:"<<x <<endl;
+    switch(ktora_funkcja) {
+      case 1:
+        cout << "Podaj A: "; cin >> a;
+        cout << "Podaj B: "; cin >> b;
+        x=(-b/a);
+        if (a==0) {
+            cout << "Funkcja przyjmuje postac: y="<<b<<" wiec jest stala, brak m0" <<endl;
+        }  else {
+            cout << "Miejsce zerowe funkcji y="<<a<<"x"<<"+b to: "<<x <<endl;
+        }
+        break;
+      case 2:
+        cout << "Podaj A: "; cin >> a;
+        cout << "Podaj B: "; cin >> b;
+        cout << "Podaj C: "; cin >> c;
+        x=(-c/a);
+        if (b==0) {
+            cout << "Funkcja przypisuje 1 y do 1 x, b nie powinno = 0" <<endl;
+        }  else {
+            cout << "Miejsce zerowe funkcji: " << a << "x + " << b <<"y + " << c << " to: " << x <<endl;
+        }
+        break;
     }
 
 
