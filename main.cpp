fbfcaf80b45f7aaa6c6823c1612f042116125d7f
mtbienias <bieniasdevs@gmail.com>
2022-11-16 16:33:44 +0100

    v1.0 - obliczanie m0 funkcji liniowej gdzie a!=0

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	
-	cout << "hello world";
+    float a, b, x;
+
+    do {
+        cout << "Program do obliczania miejsc zerowych funkcji: y=ax+b, gdzie a=/=0" <<endl;
+        cout << "Podaj A: "; cin >> a;
+        cout << "Podaj B: "; cin >> b;
+        x=(-b/a);
+        if (a==0) {
+            cout << "A nie moze byc zerem!" <<endl;
+        } else {
+            cout << "Miejsce zerowe funkcji y="<<a<<"x"<<"+b to:"<<x;
+        }
+    }
+    while (a==0);
+
 	return 0;
 }
