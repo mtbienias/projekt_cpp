59830e2e2276730d84ef7af124548ca8372d297d
mtbienias <bieniasdevs@gmail.com>
2022-11-16 16:44:02 +0100

    v1.2 mozliwosc podania a=0

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main(int argc, char** argv) {
     float a, b, x;
 
-    do {
-        cout << "Program do obliczania miejsc zerowych funkcji: y=ax+b, gdzie a=/=0" <<endl;
-        cout << "Podaj A: "; cin >> a;
-        cout << "Podaj B: "; cin >> b;
-        x=(-b/a);
-        if (a==0) {
-            cout << "A nie moze byc zerem!" <<endl;
-        } else {
-            cout << "Miejsce zerowe funkcji y="<<a<<"x"<<"+b to:"<<x;
-        }
+
+    cout << "Program do obliczania miejsc zerowych funkcji: y=ax+b" <<endl;
+    cout << "Podaj A: "; cin >> a;
+    cout << "Podaj B: "; cin >> b;
+    x=(-b/a);
+    if (a==0) {
+        cout << "Funkcja przyjmuje postac: y="<<b<<" wiec jest stala" <<endl;
+    }  else {
+        cout << "Miejsce zerowe funkcji y="<<a<<"x"<<"+b to:"<<x <<endl;
     }
-    while (a==0);
+
+
 
 	return 0;
 }
